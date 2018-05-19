036336d4599e69828b6f0a6107e184702116b42b
Ally Netzel <netzelally@gmail.com>
2018-05-19 20:29:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,14 @@
 class Dog 
   attr_accessor :name, :breed, :id 
-  @@dogs = {}
-  def initialize(name:, breed:)
-    @name = name
-    @breed = breed
-    @id = id
-  end
+#   @@dogs = {}
+#   def initialize(name:, breed:)
+#     @name = name
+#     @breed = breed
+#     @id = id
+#   end
   
-end
+# end
+# def initialize(args)
+#     args.each do |k,v|
+#       instance_variable_set("@#{k}",v) unless v.nil?
+#     end
